82eb8716d96e84f326bb41b58bde806b6912ba46
zebin <565083491@qq.com>
2019-10-04 20:02:17 +0800

    007

---
+++ main/007.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(reverse(123))
+	fmt.Println(reverse(-123))
+	fmt.Println(reverse(120))
+	fmt.Println(reverse(100000012345))
+	//fmt.Println(reverse(10000001234111111115))
+}
+
+func reverse(x int) int {
+	var sign = 1
+	if x < 0 {
+		sign = -1
+		x = -x
+	}
+	var ret = 0
+	for x != 0 {
+		ret = ret*10 + x%10
+		x /= 10
+	}
+	if sign < 0 {
+		ret = -ret
+	}
+	if ret > (1<<31-1) || ret < -(1<<31) {
+		return 0
+	}
+	return ret
+}
