fad6361ce23e4852174b339df37164b315153a11
zebin <565083491@qq.com>
2019-10-04 20:41:17 +0800

    0205

---
+++ main/0205.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isIsomorphic("egg", "add"))
+	fmt.Println(isIsomorphic("foo", "bar"))
+	fmt.Println(isIsomorphic("paper", "title"))
+}
+
+func isIsomorphic(s string, t string) bool {
+	var firstExistS = make(map[int32]int)
+	var mapS = make(map[int]int)
+	for idx, val := range s {
+		var firstIdx, ok = firstExistS[val]
+		if ok {
+			mapS[idx] = firstIdx
+		} else {
+			mapS[idx] = idx
+			firstExistS[val] = idx
+		}
+	}
+	var firstExistT = make(map[int32]int)
+	for idx, val := range t {
+		var firstIdx, ok = firstExistT[val]
+		if ok {
+			if firstIdx != mapS[idx] {
+				return false
+			}
+		} else {
+			if idx != mapS[idx] {
+				return false
+			}
+			firstExistT[val] = idx
+		}
+	}
+	return true
+}
